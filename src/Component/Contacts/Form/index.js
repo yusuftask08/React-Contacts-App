b56ecaa6554ad22c4be9f3736db103bee6fe0f71
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2022-01-18 12:50:46 +0300

    Component content added

--- src/Component/Contacts/Form/index.js
+++ src/Component/Contacts/Form/index.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
-function Form() {
+const initFormValue = { fullName: '', phone_number: '' }
+
+function Form({ addContacts, contacts }) {
+    console.log('addContact :>> ', addContacts);
+    const [form, setForm] = useState(initFormValue)
+
+    useEffect(() => {
+        setForm(initFormValue)
+    }, [contacts])
+
+    const onChangeInput = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
+    const onSubmit = (e) => {
+        e.preventDefault()
+        if (form.fullName === '' | form.phone_number === '') {
+            return false
+        }
+        addContacts([...contacts, form])
+    }
     return (
-        <div>
-            Form
-        </div>
+        <form onSubmit={onSubmit}>
+            <div>
+                <input value={form.fullName} name="fullName" placeholder='full name' onChange={onChangeInput} />
+            </div>
+            <div>
+                <input value={form.phone_number} name="phone_number" placeholder='phone number' onChange={onChangeInput} />
+            </div>
+            <div>
+                <button>Add</button>
+            </div>
+        </form>
     )
 }
 
