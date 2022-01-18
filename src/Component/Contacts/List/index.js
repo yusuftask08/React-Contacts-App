e3ad03a53e0af3316032ddcf8d29f0b953d2952a
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2022-01-18 13:06:27 +0300

    filtering process

--- src/Component/Contacts/List/index.js
+++ src/Component/Contacts/List/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useState } from 'react/cjs/react.development'
 
 function List({ contacts }) {
+    const [filterText, setFilterText] = useState('');
+    const filtered = contacts.filter((y) => {
+        return Object.keys(y).some((key) => {
+            return y[key].toString().toLowerCase().includes(filterText.toLocaleLowerCase());
+        })
+    })
+    console.log('filtered :>> ', filtered);
     return (
         <div>
+            <input placeholder='Filter Contact' value={filterText} onChange={(e) => { setFilterText(e.target.value) }} />
             {
-                contacts && contacts.map((y, i) => <li key={i}> {y.fullName} </li>)
+                filtered && filtered.map((y, i) => <li key={i}> {y.fullName} </li>)
             }
         </div>
     )
--- src/Component/Contacts/index.js
+++ src/Component/Contacts/index.js
@@ -3,7 +3,19 @@ import List from './List'
 import Form from './Form'
 
 function Contacts() {
-    const [contacts, setContacts] = useState([])
+    const [contacts, setContacts] = useState([{
+        fullName: 'yusuf',
+        phone_number: '1231231'
+    },
+    {
+        fullName: 'memo',
+        phone_number: '2222'
+    },
+    {
+        fullName: 'cevo',
+        phone_number: '111111'
+    }
+    ])
     useEffect(() => {
     }, [contacts])
     return (
