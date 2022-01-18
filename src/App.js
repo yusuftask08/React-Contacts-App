c18018543bb05671f9ab9086371738c8fb1d5a5d
yusuftask08 <52704572+yusuftask08@users.noreply.github.com>
2022-01-18 12:28:06 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Contacts from './Component/Contacts/index'
+import "./App.css"
+function App() {
+  return (
+    <div className="App">
+      <Contacts />
+      <br />
+    </div>
+  );
+}
+
+export default App;
